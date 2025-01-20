6deff211c70846c7fee08d08c3a5a658d3eab807
Emil Halling <emilhalling@gmail.com>
2025-01-20 17:09:38 +0100

    Prepare for NPM release

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Get the directory name of the current module
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
 	build: {
 		lib: {
-			entry: 'src/index.ts',
-			formats: ['es']
+			entry: path.resolve(__dirname, 'src/index.ts'),
+			name: 'ActivityGrid',
+			formats: ['es', 'umd'],
+			fileName: (format) => `activity-grid.${format}.js`
+		},
+		sourcemap: true,
+		rollupOptions: {
+			output: {
+				globals: {}
+			}
 		}
 	}
 });
